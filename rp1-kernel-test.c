0c64660b7de9338e22afdf43dc4a67dd2e33c928
Michael Bishop <cleverca22@gmail.com>
2024-01-19 18:17:56 -0400

    use physical addr for dma add more debug logs correctly handle dma_request_chan failing

--- rp1-kernel-test.c
+++ rp1-kernel-test.c
@@ -24,22 +24,27 @@ MODULE_DEVICE_TABLE(of, example_ids);
 
 static dev_t characterDevice;
 
-static int example_open(struct inode *, struct file *file) {
+static int example_open(struct inode *inode, struct file *file) {
   file->private_data = gs;
   return 0;
 }
 
-static ssize_t example_write(struct file *file, const char *data, size_t len,  loff_t *) {
+static void dma_complete(void *ptr) {
+  printk(KERN_INFO"dma complete\n");
+}
+
+static ssize_t example_write(struct file *file, const char *data, size_t len,  loff_t *offset) {
   struct example_state *state = file->private_data;
   int ret = len;
   char *buffer;
 
 #if 1
+  struct dma_async_tx_descriptor *desc;
   dma_addr_t dma;
   struct device * dev = state->chan->device->dev;
   buffer = dma_alloc_noncoherent(dev, len, &dma, DMA_TO_DEVICE, GFP_KERNEL);
 
-  printk(KERN_INFO"write %llx %ld %llx %llx\n", buffer, len, dma, dev);
+  printk(KERN_INFO"write %llx %ld %llx %llx\n", (uint64_t)buffer, len, dma, (uint64_t)dev);
   if (!buffer) return -ENOMEM;
 
   if (copy_from_user(buffer, data, len) != 0) {
@@ -49,10 +54,20 @@ static ssize_t example_write(struct file *file, const char *data, size_t len,  l
 
   dma_sync_single_for_device(dev, dma, len, DMA_TO_DEVICE);
 
-  struct dma_async_tx_descriptor *desc;
 
-  desc = dmaengine_prep_slave_single(state->chan, dma, len, DMA_MEM_TO_DEV, 0);
-  dmaengine_submit(desc);
+  desc = dmaengine_prep_slave_single(state->chan, dma, len, DMA_MEM_TO_DEV, DMA_PREP_INTERRUPT);
+  desc->callback = dma_complete;
+  desc->callback_param = NULL;
+
+  printk(KERN_INFO"phys: %llx\n", (uint64_t)desc->phys);
+  printk(KERN_INFO"chan: %llx\n", (uint64_t)desc->chan);
+  printk(KERN_INFO"tx_submit: %llx\n", (uint64_t)desc->tx_submit);
+  //printk(KERN_INFO": %x\n", desc->);
+  int cookie = dmaengine_submit(desc);
+  int retr = dma_submit_error(cookie);
+  printk(KERN_INFO"ret %d, cookie %d\n", retr, cookie);
+
+  dma_async_issue_pending(state->chan);
 
   msleep(1000);
 #else
@@ -73,7 +88,7 @@ done:
 #else
   devm_kfree(state->dev, buffer);
 #endif
-  return len;
+  return ret;
 }
 
 static int example_release(struct inode *inode, struct file *file) {
@@ -92,6 +107,15 @@ static int example_probe(struct platform_device *pdev) {
   struct example_state *state;
   struct device * dev = &pdev->dev;
   struct resource *mem;
+  struct dma_slave_config tx_conf = {
+    .dst_addr_width = DMA_SLAVE_BUSWIDTH_4_BYTES,
+    .src_addr_width = DMA_SLAVE_BUSWIDTH_4_BYTES,
+    .direction = DMA_MEM_TO_DEV,
+    .dst_maxburst = 2,
+    .dst_port_window_size = 1,
+    .device_fc = false,
+  };
+  int ret = 0;
 
   state = (struct example_state*) devm_kmalloc(dev, sizeof(struct example_state), GFP_KERNEL);
   state->dev = dev;
@@ -102,6 +126,9 @@ static int example_probe(struct platform_device *pdev) {
 
   state->regs = devm_platform_get_and_ioremap_resource(pdev, 0, &mem);
 
+  // must be the physical addr from the linux arm view, not a virt addr
+  tx_conf.dst_addr = (uint64_t)mem->start;
+
   writel('U', state->regs);
 
   state->chardev = cdev_alloc();
@@ -111,16 +138,11 @@ static int example_probe(struct platform_device *pdev) {
   state->chardev->ops = &char_fops;
 
   state->chan = dma_request_chan(dev, "tx");
-
-  struct dma_slave_config tx_conf = {
-    .dst_addr = state->regs,
-    .dst_addr_width = DMA_SLAVE_BUSWIDTH_4_BYTES,
-    .src_addr_width = DMA_SLAVE_BUSWIDTH_4_BYTES,
-    .direction = DMA_MEM_TO_DEV,
-    .dst_maxburst = 2,
-    .dst_port_window_size = 1,
-    .device_fc = true,
-  };
+  if (IS_ERR(state->chan)) {
+    ret = PTR_ERR(state->chan);
+    printk(KERN_ERR"failed to request dma channel %d\n", ret);
+    goto fail;
+  }
 
   dmaengine_slave_config(state->chan, &tx_conf);
 
@@ -133,19 +155,27 @@ static int example_probe(struct platform_device *pdev) {
 
   printk(KERN_INFO"example driver loaded\n");
   return 0;
+fail:
+  cdev_del(state->chardev);
+  unregister_chrdev_region(characterDevice, 1);
+  gs = NULL;
+  devm_kfree(dev, state);
+  return ret;
 }
 
 static int example_remove(struct platform_device *pdev) {
   struct device * dev = &pdev->dev;
   struct example_state *state = dev_get_drvdata(dev);
-  printk(KERN_INFO"example driver unloaded\n");
+  printk(KERN_INFO"example driver unloading\n");
 
   cdev_del(state->chardev);
   unregister_chrdev_region(characterDevice, 1);
+  dmaengine_terminate_sync(state->chan);
   dma_release_channel(state->chan);
 
   gs = NULL;
   devm_kfree(dev, state);
+  printk(KERN_INFO"example driver unloaded\n");
   return 0;
 }
 
